3e4fd424c0a7c321b369ba813683ce9af004aa8e
CharanKamalSingh <CharanKamalSingh@LAPTOP-LUHMM0UH>
2025-01-06 11:14:24 +0530

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,51 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Welcome to the Calculator App\n");
+    
+    Console.Write("Enter the first number: ");
+    string input = Console.ReadLine();
+    int num1 = int.Parse(input);
+
+    Console.Write("Enter the second number: ");
+    input = Console.ReadLine();
+    int num2 = int.Parse(input);
+
+    Console.WriteLine("Choose an option: ");
+    Console.WriteLine("[A]dd a number");
+    Console.WriteLine("[S]ubtract a number");
+    Console.WriteLine("[M]ultiply a number");
+
+    Console.Write("Your option: ");   
+    input = Console.ReadLine();
+
+    int result = 0; 
+    switch(input.ToLower())
+    {
+        case "a":
+            result = num1 + num2;
+            PrintResultEquation(num1, num2, "+", result);
+            break;
+
+        case "s":
+            result = num1 - num2;
+            PrintResultEquation(num1, num2, "-", result);
+            break;
+
+        case "m": 
+            result = num1 * num2;
+            PrintResultEquation(num1, num2, "*", result);
+            break;
+
+        default:
+            Console.WriteLine("Invalid Input!\n\n");
+            break;
+    }
+
+}
+
+
+void PrintResultEquation(int num1, int num2, string @operator, int result)
+{   
+    Console.WriteLine($"{num1} {@operator} {num2} = {result}");
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,163 @@
+﻿using System.Xml;
+
+List<string> todos = new List<string>();
+
+while(true)
+{
+    Console.Clear();
+    Console.WriteLine("Hello!");
+    Console.WriteLine("What do you want to do?\r\n");
+
+    Console.WriteLine("[S]ee all Todos");
+    Console.WriteLine("[A]dd a Todo");
+    Console.WriteLine("[D]elete a Todo");
+    Console.WriteLine("[U]pdate Todo");
+    Console.WriteLine("[E]xit");
+
+    char option;
+
+    Console.Write("\nChoose an option: ");
+    option = char.ToUpper(Console.ReadKey(true).KeyChar);
+    Console.Write(option);  
+
+    switch (option)
+    {
+        case 'A':
+            string todo;
+            
+            do
+            {
+                Console.Write("\nEnter a new Todo: ");
+                todo = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(todo))
+                {
+                    Console.WriteLine("The description cannot be empty.");
+                }
+
+                if(todos.Contains(todo))
+                {
+                    Console.WriteLine("The description must be unique.");
+                    continue;
+                }
+            } while (string.IsNullOrWhiteSpace(todo));
+
+
+            todos.Add(todo);
+            Console.WriteLine("TODO successfully added: " + todo);
+            break;
+
+        case 'S':
+            Console.WriteLine();
+            if(todos.Count == 0)
+            {
+                Console.WriteLine("No TODOs have been added yet.");
+            }
+            
+            int i = 1;
+            foreach(var _todo in todos)
+            {
+                Console.WriteLine($" {i}. {_todo}");
+                i++;
+            }
+
+            break;
+
+        case 'D':
+            Console.WriteLine();
+            if(todos.Count <= 0)
+            {
+                Console.WriteLine("No TODOs have been added yet.");
+                break;
+            }
+            
+            i = 1;
+            foreach (var _todo in todos)
+            { 
+                Console.WriteLine($" {i}. {_todo}");
+                i++;
+            }
+
+            int index = 0;
+            while(true)
+            {
+                Console.Write("\nSelect the index of the TODO you want to remove: ");
+                string input = Console.ReadLine();
+                
+                if(int.TryParse(input, out index) && index > 0 && index <= todos.Count)
+                {
+                    break;
+                }
+
+                Console.WriteLine("The given index is not valid.");
+            }
+            
+            var todoDel = todos[index - 1];
+            todos.RemoveAt(index - 1);
+
+            Console.WriteLine("TODO removed: " + todoDel);
+            break;
+
+        case 'U':
+            if (todos.Count <= 0)
+            {
+                Console.WriteLine("No TODOs to Update");
+                break;
+            }
+
+            i = 1;
+            foreach (var _todo in todos)
+            {
+                Console.WriteLine($" {i}. {_todo}");
+                i++;
+            }
+
+            index = 0;
+            while (true)
+            {
+                Console.Write("\nEnter the todo index to be updated: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out index) && index > 0 && index <= todos.Count)
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid Index!");
+            }
+
+            string todoEdit;
+            do
+            {
+                Console.Write("\nEnter a the new Todo: ");
+                todoEdit= Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(todoEdit))
+                {
+                    Console.WriteLine("Cannot enter an empty TODO!");
+                }
+            } while (string.IsNullOrWhiteSpace(todoEdit));
+
+            todos[index - 1] = todoEdit;
+            Console.WriteLine("TODO updated successfully!");
+            break;
+
+        case 'E':
+            Environment.Exit(0);
+            break;
+
+        default:
+            Console.WriteLine("\nIncorrect input");
+            break;
+    }
+
+    WaitForKey();
+}
+
+
+void WaitForKey()
+{
+    Console.WriteLine();
+    Console.Write("Press any Key to continue...");
+    Console.ReadKey();
+}
